b2d98552cd1e4bd0ab3b4e0d379766a62291e8b5
iamlydial <lydia.lavecchia@gmail.com>
2024-08-08 14:02:49 +0200

    edits
    - correct syntax error
    - add use Session
    - correct useEffect
    - add correct next config setu

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -8,17 +8,19 @@ import { useState, useEffect } from "react";
 import { signIn, signOut, useSession, getProviders } from "next-auth/react";
 
 const Nav = () => {
-  const isUserLoggedIn = true;
-  const [providers, setproviders] = useState(null);
+  const { data: session } = useSession();
+  const [providers, setProviders] = useState(null);
   const [toggleDropdown, setToggleDropdown] = useState(false);
 
   useEffect(() => {
-    const setProviders = async () => {
+    const setUpProviders = async () => {
       const response = await getProviders();
+
       setProviders(response);
     };
-    setProviders();
+    setUpProviders();
   }, []);
+
   return (
     <nav className="flex-between w-full pt-3 mb-16">
       <Link href="/" className="flex gap-2 flex-center">
@@ -34,7 +36,7 @@ const Nav = () => {
 
       {/* Desktop navigation */}
       <div className="sm:flex hidden">
-        {isUserLoggedIn ? (
+        {session?.user ? (
           <div className="flex gap-3 md:gap-5">
             <Link href="/create-prompt" className="black_btn">
               Create Post
@@ -70,7 +72,7 @@ const Nav = () => {
       </div>
       {/* mobile navigation  */}
       <div className="sm:hidden flex relative">
-        {isUserLoggedIn ? (
+        {session?.user ? (
           <div className="flex">
             <Image
               src="/assets/images/logo.svg"
