16dcf97e8fa6af44455e6ed36b5d19153a9745f6
iamlydial <lydia.lavecchia@gmail.com>
2024-08-24 19:37:32 +0200

    edit home and lauoyt pages

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,28 +2,27 @@ import "../styles/globals.css";
 import Nav from "../components/Nav";
 import Provider from "../components/Provider";
 
+
 export const metadata = {
   title: "Promptopia",
-  description: "Discover and Share AI Prompts",
+  description: "Discover & Share AI Prompts",
 };
 
-const RootLayout = ({ children }) => {
-  return (
-    <html>
-      <body>
-        <Provider>
-          <div className="main">
-            <div className="gradient" />
-          </div>
+const RootLayout = ({ children }) => (
+  <html lang='en'>
+    <body>
+      <Provider>
+        <div className='main'>
+          <div className='gradient' />
+        </div>
 
-          <main className="app">
-            <Nav />
-            {children}
-          </main>
-        </Provider>
-      </body>
-    </html>
-  );
-};
+        <main className='app'>
+          <Nav />
+          {children}
+        </main>
+      </Provider>
+    </body>
+  </html>
+);
 
 export default RootLayout;
