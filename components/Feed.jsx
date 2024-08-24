55e5c9d77e3250e4c52d6c08f0f39dd99a08f163
iamlydial <lydia.lavecchia@gmail.com>
2024-08-24 15:52:03 +0200

    Update Feed.jsx

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,7 +1,52 @@
-import React from "react";
+"use client";
+
+import { useState, useEffect } from "react";
+import PromptCard from "./PromptCard";
+
+const PrompCardList = ({ data, handleTagClick }) => {
+  return (
+    <div className="mt-16 prompt_layout">
+      {data.map((post) => {
+        <PromptCard
+          key={post.id}
+          post={post}
+          handleTagClick={handleTagClick}
+        />;
+      })}
+    </div>
+  );
+};
 
 const Feed = () => {
-  return <div>FEED</div>;
+  const [searchText, setSearchText] = useState("");
+  const [posts, setPosts] = useState([]);
+
+  const handleSearchChange = (e) => {};
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch("/api/prompt");
+      const data = await response.json();
+      setPosts(data);
+    };
+    fetchPosts();
+  }, []);
+
+  return (
+    <section className="feed">
+      <form className="relative w-full flex-center">
+        <input
+          title="text"
+          placeholder="Search for a tag or a username"
+          value={searchText}
+          onChange={handleSearchChange}
+          required
+          className="search_input peer"
+        />
+        <PrompCardList data={[posts]} handleTagClick={() => {}} />
+      </form>
+    </section>
+  );
 };
 
 export default Feed;
