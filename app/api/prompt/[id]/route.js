8f9d40db8fe7efd7736f2b5a78b570b121513bd4
iamlydial <lydia.lavecchia@gmail.com>
2024-08-24 18:18:37 +0200

    Create route.js
    - create a new route to maipulate single post end points GET, PATCH, DELETE.
    - add get route

---
+++ app/api/prompt/[id]/route.js
@@ -0,0 +1,24 @@
+//get
+import { connectToDB } from "../../../../utils/database";
+import Prompt from "../../../../models/prompt";
+
+export const GET = async (request, { params }) => {
+  try {
+    console.log("connected to DB");
+    const prompt = await Prompt.findById(params.id).populate("creator");
+
+    if (!prompt) {
+      return new Response("Prompt not found", { status: 404 });
+    }
+
+    return new Response(JSON.stringify(prompt), { status: 200 });
+  } catch (error) {
+    return new Response("Internal Server Error", {
+      status: 500,
+    });
+  }
+};
+
+//patch
+
+//delete
