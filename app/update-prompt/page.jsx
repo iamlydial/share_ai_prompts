acdd2e8cedbf4dd234e8792121664162d29632a8
iamlydial <lydia.lavecchia@gmail.com>
2024-08-24 19:49:29 +0200

    Update page.jsx

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -1,44 +1,37 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import { useRouter } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
+
 import Form from "../../components/Form";
 
 const UpdatePrompt = () => {
   const router = useRouter();
-  const [post, setPost] = useState({ prompt: "", tag: "" });
-  const [submitting, setIsSubmitting] = useState(false);
-  const [loading, setLoading] = useState(true);
+  const searchParams = useSearchParams();
+  const promptId = searchParams.get("id");
 
-  // Get the promptId from the query string
-  const promptId = router.query.id; 
+  const [post, setPost] = useState({ prompt: "", tag: "", });
+  const [submitting, setIsSubmitting] = useState(false);
 
   useEffect(() => {
-    if (promptId) {
-      const getPromptDetails = async () => {
-        try {
-          const response = await fetch(`/api/prompt/${promptId}`);
-          if (!response.ok) throw new Error("Failed to fetch prompt details");
-          const data = await response.json();
-          setPost({ prompt: data.prompt, tag: data.tag });
-        } catch (error) {
-          console.error(error);
-        } finally {
-          setLoading(false);
-        }
-      };
-      getPromptDetails();
-    }
+    const getPromptDetails = async () => {
+      const response = await fetch(`/api/prompt/${promptId}`);
+      const data = await response.json();
+
+      setPost({
+        prompt: data.prompt,
+        tag: data.tag,
+      });
+    };
+
+    if (promptId) getPromptDetails();
   }, [promptId]);
 
   const updatePrompt = async (e) => {
     e.preventDefault();
     setIsSubmitting(true);
 
-    if (!promptId) {
-      alert("Missing PromptId!");
-      return;
-    }
+    if (!promptId) return alert("Missing PromptId!");
 
     try {
       const response = await fetch(`/api/prompt/${promptId}`, {
@@ -47,27 +40,21 @@ const UpdatePrompt = () => {
           prompt: post.prompt,
           tag: post.tag,
         }),
-        headers: {
-          "Content-Type": "application/json",
-        },
       });
 
-      if (!response.ok) throw new Error("Failed to update prompt");
-      router.push("/");
+      if (response.ok) {
+        router.push("/");
+      }
     } catch (error) {
-      console.error(error);
+      console.log(error);
     } finally {
       setIsSubmitting(false);
     }
   };
 
-  if (loading) {
-    return <div>Loading prompt details...</div>;
-  }
-
   return (
     <Form
-      type="Edit"
+      type='Edit'
       post={post}
       setPost={setPost}
       submitting={submitting}
